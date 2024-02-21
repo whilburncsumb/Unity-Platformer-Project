2bff0ca708bf2a591363a8cb5144328b1916380a
William <whilburn@csumb.edu>
2024-02-20 19:38:14 -0800

    In-class updates to platformer

--- Assets/Platformer/Scripts/CharacterControllerLive.cs
+++ Assets/Platformer/Scripts/CharacterControllerLive.cs
@@ -1,12 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterControllerLive : MonoBehaviour
 {
-    public float speed = 250f;
-    public float maxSpeed = 10f;
-    public float jumpForce;
+    public float speed = 50f;
+    public float maxSpeed = 15f;
+    public float jumpForce = 10f;
+    public float jumpBoost = .3f;
     public bool grounded;
     private Rigidbody rbody;
 
@@ -23,13 +25,15 @@ public class CharacterControllerLive : MonoBehaviour
     void Update()
     {
         float horizontalMovement = Input.GetAxis("Horizontal");
+        // rbody.velocity *= Math.Abs(horizontalMovement);
+        
         
         rbody.velocity += Vector3.right *horizontalMovement *Time.deltaTime *speed;
-        if (rbody.velocity.x > maxSpeed)
+        if (Math.Abs(rbody.velocity.x) > maxSpeed)
         {
             // rbody.velocity = rbody.velocity.normalized * maxSpeed;
             Vector3 newV = rbody.velocity;
-            newV.x = maxSpeed;
+            newV.x = Mathf.Clamp(newV.x,-maxSpeed, maxSpeed);
             rbody.velocity = newV;
         }
 
@@ -43,10 +47,25 @@ public class CharacterControllerLive : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
             rbody.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
+        } else if (!grounded && Input.GetKey(KeyCode.Space))
+        {
+            if (rbody.velocity.y > 0)
+            {
+                rbody.AddForce(Vector3.up*jumpBoost,ForceMode.Force);
+            }
+            
         }
 
+        if (Math.Abs(horizontalMovement)<0.5f)
+        {
+            // rbody.velocity *= Math.Abs(horizontalMovement);
+            Vector3 newV = rbody.velocity;
+            newV.x *= 1f - Time.deltaTime;
+            rbody.velocity = newV;
+        }
+        float yaw = (rbody.velocity.x>0) ? 90:-90;
+        // transform.rotation.eulerAngles = new Vector3(0f,yaw,0f);
+        transform.rotation = Quaternion.Euler(0f, yaw, 0f);
 
-
-        
     }
 }
